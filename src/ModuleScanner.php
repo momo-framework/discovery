3b072adcd7d9b73439527f386045be1b8c5053cb
Vahe Sargsyan <w33bv.gl@gmail.com>
2026-03-15 18:49:49 +0300

    add license

--- src/ModuleScanner.php
+++ src/ModuleScanner.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of Momo Framework.
+ *
+ * @copyright Vahe Sargsyan
+ * @license   AGPL-3.0-or-later <https://www.gnu.org/licenses/agpl-3.0.html>
+ * @link      https://github.com/momo-framework
+ */
+
 declare(strict_types=1);
 
 namespace Momo\Discovery;
